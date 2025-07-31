f7d46efd20f7efe5aac55262bb0aff4da2bd5af7
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-07-31 21:34:01 +0530

    added models , services and controller packages and their implementation

---
+++ src/main/java/com/ashwani/secure/notes/security/services/UserDetailsImpl.java
@@ -0,0 +1,33 @@
+package com.ashwani.secure.notes.security.services;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+    private static final  long serialVersionUID= 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
